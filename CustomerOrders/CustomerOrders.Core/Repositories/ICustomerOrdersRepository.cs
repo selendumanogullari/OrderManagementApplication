72eb57555dda62fb10d0fcb95961a424ba237a59
esdum <esdum@SELO>
2025-01-22 01:51:38 +0300

    fix build errors

--- CustomerOrders/CustomerOrders.Core/Repositories/ICustomerOrdersRepository.cs
+++ CustomerOrders/CustomerOrders.Core/Repositories/ICustomerOrdersRepository.cs
@@ -1,9 +1,15 @@
-﻿using static CustomerOrders.Core.Repositories.Base.IRepository;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CustomerOrders.Core.Repositories.Base.IRepository;
 
 namespace CustomerOrders.Core.Repositories
 {
     public interface ICustomerOrdersRepository : IRepository<CustomerOrders.Core.Entities.CustomerOrders>
     {
         Task<List<CustomerOrders.Core.Entities.CustomerOrders>> GetByCustomerAndOrderAsync(int customerId);
+
     }
 }
